d98af9079963f4bed953cb89ee663befca150dc2
Brian Flad <bflad417@gmail.com>
2014-04-07 20:52:25 -0400

    Rubocop automatic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name              'lsyncd'
-maintainer        'Brian Flad'
-maintainer_email  'bflad417@gmail.com'
-license           'Apache 2.0'
-description       'Installs/configures lsyncd.'
-version           '0.1.2'
-recipe            'lsyncd', 'Installs/configures lsyncd.'
-recipe            'lsyncd::source', 'Installs lsyncd from source.'
+name 'lsyncd'
+maintainer 'Brian Flad'
+maintainer_email 'bflad417@gmail.com'
+license 'Apache 2.0'
+description 'Installs/configures lsyncd.'
+version '0.1.2'
+recipe 'lsyncd', 'Installs/configures lsyncd.'
+recipe 'lsyncd::source', 'Installs lsyncd from source.'
 
-%w{ amazon centos redhat scientific }.each do |os|
+%w(amazon centos redhat scientific).each do |os|
   supports os
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@ package 'lsyncd'
 
 template '/etc/init.d/lsyncd' do
   source 'lsyncd.init.erb'
-  mode   '0755'
+  mode '0755'
 end
 
 service 'lsyncd' do
@@ -12,8 +12,8 @@ end
 
 template '/etc/lsyncd.lua' do
   source 'lsyncd.lua.erb'
-  owner  'root'
-  group  'root'
-  mode   '0644'
+  owner 'root'
+  group 'root'
+  mode '0644'
   notifies :restart, 'service[lsyncd]', :delayed
 end
